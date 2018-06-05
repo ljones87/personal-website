c8daba0a3e92550b9a37f68443f385b70b928d56
Lina Jones <lina.jones630@gmail.com>
2018-06-05 15:57:36 -0600

    dev icons now in flexbox w/in grid

--- app/Components/TechStack.js
+++ app/Components/TechStack.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const TechStack = () => {
   return (
@@ -15,23 +15,25 @@ const TechStack = () => {
         <h1 className="tech-header">
           The more you know!
         </h1>
-        <h3 className="sub-header">
+        <h4 className="sub-header">
         Where my skillz at...
            <div className="underline" />
-        </h3>
-        <i className="devicon-javascript-plain colored dev-1" />
-        <i className="devicon-react-original colored dev-2" />
-        <i className="devicon-nodejs-plain-wordmark colored dev-3" />
-        <i className="devicon-github-plain colored dev-4" />
-        <i className="devicon-html5-plain colored dev-5" />
-        <i className="devicon-sass-original colored dev-6" />
-        <i className="devicon-mocha-plain colored dev-7" />
-        <i className="devicon-postgresql-plain colored dev-8" />
-        <i className="devicon-sequelize-plain colored dev-9" />
-        <i className="devicon-babel-plain colored dev-10" />
-        <i className="devicon-webpack-plain colored dev-11" />
-        <i className="devicon-d3js-plain colored dev-12" />
-        {/*<i className="devicon-docker-plain colored" />*/}
+        </h4>
+        <div className="tech-icos">
+          <i className="devicon-javascript-plain colored dev-1" />
+          <i className="devicon-react-original colored dev-2" />
+          <i className="devicon-nodejs-plain-wordmark colored dev-3" />
+          <i className="devicon-github-plain colored dev-4" />
+          <i className="devicon-html5-plain colored dev-5" />
+          <i className="devicon-sass-original colored dev-6" />
+          <i className="devicon-mocha-plain colored dev-7" />
+          <i className="devicon-postgresql-plain colored dev-8" />
+          <i className="devicon-sequelize-plain colored dev-9" />
+          <i className="devicon-babel-plain colored dev-10" />
+          <i className="devicon-webpack-plain colored dev-11" />
+          <i className="devicon-d3js-plain colored dev-12" />
+          {/*<i className="devicon-docker-plain colored" />*/}
+        </div>
       </div>
     </div>
   );
