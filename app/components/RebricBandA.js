d4ac86fb143a35576ab35f905d70819d7286acd7
Lina Jones <lina.jones630@gmail.com>
2018-08-10 22:08:20 -0600

    (wip) flex view for video page

--- app/components/RebricBandA.js
+++ app/components/RebricBandA.js
@@ -5,21 +5,25 @@ const RebricBandA = (props) => {
   return (
     <div className="videos">
       <a href="http://rebric.io"
-      target="_blank"
-      rel="noopener noreferrer"
+        target="_blank"
+        rel="noopener noreferrer"
       >
-      <h1>Rebric.io</h1>
+        <h1>Rebric.io</h1>
       </a>
-      <h3 className="before">Before Conversion</h3> <h3  className="after">After Conversion</h3>
-      <video
-        className="rebric-before"
-        src={'/public/videos/RebricBefore.mov'}
-        controls />
-      <video
-        className="rebric-after"
-        src={'/public/videos/RebricAfter.mov'}
-        controls />
-
+      <div className="before">
+        <h3 >Before Conversion</h3>
+        <video
+          className="rebric-before"
+          src={'/public/videos/RebricBefore.mov'}
+          controls />
+      </div>
+      <div className="after">
+        <h3 >After Conversion</h3>
+        <video
+          className="rebric-after"
+          src={'/public/videos/RebricAfter.mov'}
+          controls />
+      </div>
     </div>
   );
 };
--- app/components/TechStack.js
+++ app/components/TechStack.js
@@ -5,14 +5,14 @@ const TechStack = () => {
      <div className="tech">
       <a name="tech" />
         <div className="rainbow">
-          <span className="pink" />
-          <span className="purple" />
           <span className="red" />
+          <span className="orange" />
+          <span className="yellow" />
           <span className="green" />
           <span className="star" />
         </div>
         <h1 className="tech-header">
-          The more you know!
+          Tech I use often
         </h1>
 
         <div className="tech-icos">
@@ -20,13 +20,13 @@ const TechStack = () => {
           <i className="devicon-react-original colored dev-2" />
           <i className="devicon-nodejs-plain-wordmark colored dev-3" />
           <i className="devicon-github-plain colored dev-4" />
-          <i className="devicon-html5-plain colored dev-5" />
-          <i className="devicon-sass-original colored dev-6" />
-          <i className="devicon-mocha-plain colored dev-7" />
-          <i className="devicon-postgresql-plain colored dev-8" />
-          <i className="devicon-sequelize-plain colored dev-9" />
-          <i className="devicon-babel-plain colored dev-10" />
-          <i className="devicon-webpack-plain colored dev-11" />
+          <i className="devicon-webpack-plain colored dev-5" />
+          <i className="devicon-html5-plain colored dev-6" />
+          <i className="devicon-sass-original colored dev-7" />
+          <i className="devicon-mocha-plain colored dev-8" />
+          <i className="devicon-postgresql-plain colored dev-9" />
+          <i className="devicon-sequelize-plain colored dev-10" />
+          <i className="devicon-babel-plain colored dev-11" />
           <i className="devicon-d3js-plain colored dev-12" />
           {/*<i className="devicon-docker-plain colored" />*/}
         </div>
